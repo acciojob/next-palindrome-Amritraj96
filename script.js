68e5036955cc08c91effe9a0dd9ba81daf418d72
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-06-13 22:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  // Convert input to a number and start checking from the next integer
+  let nextNum = Number(num) + 1;
+
+  // Helper function to check if a number is a palindrome
+  function isPalindrome(n) {
+    const str = n.toString();
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+  }
+
+  // Loop until we find the next palindrome
+  while (!isPalindrome(nextNum)) {
+    nextNum++;
+  }
+
+  return nextNum;
 }
 
+// Taking input and showing output as per instructions
 const input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
